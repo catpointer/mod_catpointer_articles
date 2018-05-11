cd8ddcec3c1d7ba60d7f5d99eb55afb3e7efba9a
Roberto Segura - phproberto <roberto@phproberto.com>
2018-05-11 17:36:50 +0200

    [ci] travis integration

---
+++ ci/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Clone current joomla & install composer there
+git clone https://github.com/joomla/joomla-cms.git /tmp/joomla-cms
+composer install -d /tmp/joomla-cms
+
+# Install extension composer dependencies
+composer install -d ./extensions/modules/site/mod_catpointer_articles --prefer-dist --no-interaction --no-progress
+
+# Copy CI gulp config (with cloned joomla path) to the build folder
+cp ./ci/gulp-config.ci.json ./build/gulp-config.json
+
+# Install npm packages in the build folder
+cd ./build
+npm install -g gulp 
+npm install
+
+# Copy joomla extensions to the cloned joomla site
+gulp copy 
+cd ..
