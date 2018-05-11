05b465cb6f761e34bc8b02f371decf80982d25a2
Roberto Segura - phproberto <roberto@phproberto.com>
2018-05-11 17:41:44 +0200

    [fix] missing composer deps

--- ci/setup.sh
+++ ci/setup.sh
@@ -5,7 +5,10 @@ git clone https://github.com/joomla/joomla-cms.git /tmp/joomla-cms
 composer install -d /tmp/joomla-cms
 
 # Install extension composer dependencies
-composer install -d ./extensions/modules/site/mod_catpointer_articles --prefer-dist --no-interaction --no-progress
+composer install --prefer-dist --no-interaction --no-progress
+
+# Install extension composer dependencies
+composer install -d ./extensions/modules/site/mod_catpointer_articles
 
 # Copy CI gulp config (with cloned joomla path) to the build folder
 cp ./ci/gulp-config.ci.json ./build/gulp-config.json
